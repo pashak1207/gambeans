365efc52c8b9d09fbab64dea10e7870e78511207
root <root@Pasha>
2023-08-30 19:00:28 +0300

    Prisma setup

---
+++ prisma/migrations/20230823153704_remove_cafe_code_number/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `code_phone` on the `Cafes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Cafes` DROP COLUMN `code_phone`;
